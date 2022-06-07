2713f0a2c671dfb10a4f0d997da7d77da1676d35
mohamed-dahni <mdahni06@gmail.com>
2022-06-07 10:56:28 +0100

    initial commit

---
+++ js/bar.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    menu = document.querySelector("#menu");
+    links = document.querySelector(".links");
+
+    links.addEventListener('click', function() {
+        if (menu.className === "menu"){
+            menu.className += " disapear";
+        } else {
+            menu.className = "menu";
+        }
+    });
+});
